48be24a7cc8db134a39615f6945e0a4a0b3f3bf5
bankdaysun <136328375+bankdaysun@users.noreply.github.com>
2025-07-26 00:45:44 +0700

    Update 01_MQTT_pub.py

--- 01_MQTT_pub.py
+++ 01_MQTT_pub.py
@@ -1,25 +1,12 @@
 import paho.mqtt.client as mqtt
 import time
 
-broker = "broker.emqx.io"
+broker = "_______________"
 client = mqtt.Client()
 client.connect(broker, 1883)
 
 while True:
-    
-    client.publish("idt/4/led1", "on")
-    client.publish("idt/4/led2", "on")
-    client.publish("idt/4/led3", "on")
-    client.publish("idt/4/led4", "on")
-    client.publish("idt/4/led5", "on")
-    client.publish("idt/4/led6", "on")
-    time.sleep(1)
-    client.publish("idt/4/led1", "off")
-    client.publish("idt/4/led2", "off")
-    client.publish("idt/4/led3", "off")
-    client.publish("idt/4/led4", "off")
-    client.publish("idt/4/led5", "off")
-    client.publish("idt/4/led6", "off")
+    client.publish("topic", 22.4)
     time.sleep(1)
 
 
